a5355c94847d0cb04ce7a66393fdb689fb8a0850
tomcoolpxl <71908551+tomcoolpxl@users.noreply.github.com>
2022-10-19 21:08:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flask-pxl
+ 
---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+import logging
+
+app = Flask(__name__)
+
+# log time
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+backend_network = os.environ.get('BACKEND_NETWORK', '0.0.0.0')
+backend_port = os.environ.get('BACKEND_PORT', '5000')
+
+
+@app.route('/')
+def hello():
+    return 'Hello, PXL!'
+
+
+if __name__ == "__main__":
+    logging.info('starting backend server on ' +
+                 backend_network + ' port ' + backend_port)
+    app.run(host=backend_network, port=backend_port)
